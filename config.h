bf753ecaa1bf2bab35eb9f62db4dcb298bc19303
Jesse Spielman <jesse.spielman@gmail.com>
2016-08-01 11:57:31 +0100

    Change lock command to xscreensaver-command

--- config.h
+++ config.h
@@ -59,7 +59,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "xterm", NULL };
 // TODO: add mutt, chromium commands
-static const char *lockmonitor[]  = { "slock", NULL };
+static const char *lockmonitor[]  = { "xscreensaver-command", "-activate", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
