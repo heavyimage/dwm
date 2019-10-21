a7a66b4f338360e11477454e722b63eb9dbe4455
Jesse Spielman <jesse.spielman@gmail.com>
2019-10-21 01:21:24 +0100

    Some cosmetic changes

--- config.h
+++ config.h
@@ -3,7 +3,7 @@
 #include <X11/XF86keysym.h>
 
 /* appearance */
-static const unsigned int borderpx  = 4;        /* border pixel of windows */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
@@ -12,21 +12,21 @@ static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=14" };
-static const char dmenufont[]       = "monospace:size=14";
+static const char dmenufont[]       = { "monospace:size=14" };
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
+static const char col_black[]       = "#000000";
 static const char col_cyan[]        = "#005577";
-static const char col_smoke[]       = "#2c3144";
+static const char col_carcosa[]     = "#cccc00";
 static const char *colors[][3]      = {
         /*               fg         bg         border   */
         [SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-        [SchemeSel]  = { col_gray4, col_smoke, col_cyan },
+        [SchemeSel]  = { col_black, col_carcosa, col_carcosa },
 };
 
 /* tagging */
-static const char *tags[] = { "c&c", "web", "mail", "todos", "dwm", "social", "shell", "shell", "shell"};
+static const char *tags[] = { "c&c", "web", "mail", "skyvault", "social", "shell", "shell", "shell", "shell"};
 
 static const Rule rules[] = {
 	/* xprop(1):
